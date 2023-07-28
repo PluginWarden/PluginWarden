d98a635f003f9c2edce124f2375d2d1f9f45cc45
Chaoscaot <chaoscaot@zohomail.eu>
2023-07-28 16:35:35 +0200

    Finish InstallCommand

--- src/main/kotlin/de/pluginwarden/commands/InstallCommand.kt
+++ src/main/kotlin/de/pluginwarden/commands/InstallCommand.kt
@@ -12,11 +12,22 @@ import kotlinx.cli.ArgType
 import kotlinx.cli.Subcommand
 import kotlinx.cli.default
 import kotlinx.cli.vararg
+import java.io.File
+import java.net.URL
 
 object InstallCommand: Subcommand("install", "Installs a plugin") {
     private val plugin by argument(ArgType.String, description = "The plugin to install").vararg()
     private val force by option(ArgType.Boolean, description = "Force the installation of the plugin", shortName = "f").default(false)
     private val yes by option(ArgType.Boolean, description = "Answer yes to all questions", shortName = "y").default(false)
+
+    fun download(url: String): File {
+        val tmpFile = File.createTempFile("pluginwarden", ".jar")
+        tmpFile.deleteOnExit()
+        t.println("Downloading ${green(url)}...")
+        URL(url).openStream().transferTo(tmpFile.outputStream())
+        t.println("Downloaded ${green(url)}!")
+        return tmpFile
+    }
 
     override fun execute() {
         updatePluginStorage()
@@ -69,6 +80,90 @@ object InstallCommand: Subcommand("install", "Installs a plugin") {
             }.let {
                 if (it == "No") return
             }
+        }
+
+        plToInstall.forEach {
+            val installedPlugin = pluginsList?.find { plugin -> it.second.prefixes.any { plugin.file.nameWithoutExtension.startsWith(it, true) } }
+            if (installedPlugin != null) {
+                t.println("Plugin ${red(it.second.prefixes.first())} is already installed!")
+                return
+            }
+            val file = it.first.storagePluginDownloads.firstOrNull { dl -> dl.serverType == serverType } ?: it.first.storagePluginDownloads.first()
+            val filesToRemove = mutableListOf<File>()
+            val tmp = download(file.link)
+
+            val deps = it.first.storagePluginDependencies.map { d ->
+                fun downloadDep(name: String, version: Version): File {
+                    val p = getPluginStoragePlugin(name)
+                    if (p == null) {
+                        t.println("Plugin ${red(name)} not found!")
+                        throw Exception("Plugin ${red(name)} not found!")
+                    }
+                    val v = p.versions.firstOrNull { it.version == version && (it.isCompatible() || force) }
+                    if (v == null) {
+                        t.println("Version ${red(version.toString())} of plugin ${red(p.prefixes.joinToString(" "))} not found!")
+                        throw Exception("Version ${red(version.toString())} of plugin ${red(p.prefixes.joinToString(" "))} not found!")
+                    }
+                    val file = v.storagePluginDownloads.firstOrNull { dl -> dl.serverType == serverType } ?: v.storagePluginDownloads.first()
+                    return download(file.link)
+                }
+
+                val installedDeps = pluginsList!!.filter { ip -> d.dependencies.any { dd -> dd.key == ip.name } }
+
+                installedDeps.forEach { ip ->
+                    if(d.dependencies[ip.name] == ip.version) {
+                        t.println("Dependency ${red(ip.name)} is already installed!")
+                        return@map null
+                    } else {
+                        if(!yes && !force) {
+                            t.println("Dependency ${red(ip.name)} is already installed, but the wrong version!")
+                            t.println("  Installed: ${ip.version}")
+                            t.println("  Required: ${d.dependencies[ip.name]}")
+                            t.prompt("Do you want to update the dependency?", choices = listOf("Yes", "No"), default = "Yes"){
+                                if (it.startsWith("n")) ConversionResult.Valid("No")
+                                else if (it.startsWith("y") || it.startsWith("j")) ConversionResult.Valid("Yes")
+                                else ConversionResult.Invalid("Invalid choice")
+                            }.let {
+                                if (it == "No") return@map null
+                                else filesToRemove.add(ip.file)
+                            }
+                        }
+                    }
+                }
+
+                if(d.dependencies.size == 1) {
+                    downloadDep(d.dependencies.entries.first().key, d.dependencies.entries.first().value) to d.dependencies.entries.first()
+                } else {
+                    t.println("Download Dependency ${red(d.groupName)}")
+                    d.dependencies.entries.forEachIndexed { index, entry ->
+                        t.println("  ${index + 1}. ${entry.key}:${entry.value}")
+                    }
+
+                    val dInstall = t.prompt("Which dependency do you want to download?", default = "1") {
+                        val i = it.toIntOrNull()
+                        if(i == null) ConversionResult.Invalid("Invalid choice")
+                        else if(i < 1 || i > d.dependencies.size) ConversionResult.Invalid("Invalid choice")
+                        else ConversionResult.Valid(d.dependencies.entries.toList()[i - 1].key)
+                    }
+
+                    downloadDep(dInstall!!, d.dependencies[dInstall]!!) to d.dependencies.entries.first { it.key == dInstall }
+                }
+            }.filterNotNull()
+
+            t.println("Installing ${green(it.second.prefixes.first())}...")
+
+            filesToRemove.forEach { f ->
+                t.println("Removing ${f.name}...")
+                f.delete()
+            }
+
+            deps.forEach { (f, k) ->
+                f.copyTo(File(pluginsDirectory, "${k.key}-${k.value}.jar"))
+            }
+
+            tmp.copyTo(File(pluginsDirectory, "${it.second.prefixes.first()}-${it.first.version}.jar"))
+
+            t.println("Installed ${green(it.second.prefixes.first())}!")
         }
     }
 }
